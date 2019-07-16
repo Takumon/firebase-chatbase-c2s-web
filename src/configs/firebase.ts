c18c8a396c86d8ba2d40bbcc6c5b1099e47b7e3d
Takumon <inouetakumon@gmail.com>
2019-07-16 14:27:30 -0700

    try firestore

--- src/configs/firebase.ts
+++ src/configs/firebase.ts
@@ -10,4 +10,19 @@ firebase.initializeApp({
   appId: "1:26135180478:web:55804112737d8402"
 });
 
+export const firestore = firebase.firestore();
+firestore.settings({
+  timestampsInSnapshots: true
+});
+
+firestore.collection("users").add({
+  first: "Adasas",
+  last: "Lovelace",
+  born: 1815
+}).then(function(docRef) {
+  console.log("Document written with ID: ", docRef.id);
+}).catch(function(error) {
+  console.error("Error adding document: ", error);
+});
+
 export default firebase;
